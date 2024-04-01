3213f7ac297389b6ee53c396228e903766fe1520
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:42:44 -0400

    adicionado questao 5

---
+++ questao5.js
@@ -0,0 +1,7 @@
+/*
+5. Qual é o resultado de (8 !== 8) || (3 <= 3)?
+(8 !== 8): Aqui, estamos verificando se 8 não é estritamente igual a 8. No entanto, 8 é igual a 8, então a expressão 8 !== 8 é falsa.
+(3 <= 3): Aqui, estamos verificando se 3 é menor ou igual a 3. Como 3 é de fato igual a 3, esta expressão é verdadeira.
+(8 !== 8) é falso.
+(3 <= 3) é verdadeiro.
+*/

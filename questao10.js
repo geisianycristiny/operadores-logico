7cd4ad65bb51b20c34ac81adfd710be67e31931f
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:44:01 -0400

    adicionado questao 10

---
+++ questao10.js
@@ -0,0 +1,6 @@
+/*
+10.Qual é o resultado de !true || false && true?
+Primeiro, tratamos a negação !true, que é false.
+Em seguida, avaliamos a conjunção false && true, que é false.
+Por fim, consideramos a disjunção false || false, que é false.
+*/

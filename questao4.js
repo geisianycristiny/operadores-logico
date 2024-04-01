8ccd5ec2824730de633d3637671843628ed04df9
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:42:31 -0400

    adicionado questao 4

---
+++ questao4.js
@@ -0,0 +1,7 @@
+/*
+4. Explique o que significa a expressão !(true && false)
+
+Dentro dos parênteses, temos true && false. Isso significa que estamos verificando se ambos os operandos são verdadeiros. No entanto, neste caso, um deles é false, então a expressão true && false resulta em false.
+Em seguida, o operador ! (negação) é aplicado ao resultado da expressão dentro dos parênteses. Isso inverte o valor, transformando false em true e vice-versa.
+Portanto, !(true && false) é equivalente a dizer "não (true e false)", o que resulta em true. Em resumo, a expressão significa que não é verdade que ambos os operandos sejam verdadeiros (o que é falso), então o resultado é verdadeiro.
+*/

cff61b6cb7be05f61f9d4f2a80f6315c36953f8d
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:41:58 -0400

    adicionado questao2

---
+++ questao2.js
@@ -0,0 +1,7 @@
+/*
+2. O que retorna a expressão (true || false) && (false || true)?
+
+(true || false) avalia para true, pois pelo menos um dos operandos é verdadeiro.
+(false || true) também avalia para true, pelo mesmo motivo.
+Em seguida, a expressão é avaliada como true && true, o que resulta em true, pois ambos os operandos são verdadeiros.
+*/

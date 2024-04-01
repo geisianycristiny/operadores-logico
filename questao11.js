42e4c88cecc21038e6657ce8c850c33bdef0469e
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:44:12 -0400

    adicionado questao 11

---
+++ questao11.js
@@ -0,0 +1,9 @@
+/*
+11.Explique a diferença entre os operadores && e || em JavaScript
+O operador && retorna true se e somente se ambos os operandos forem verdadeiros.
+Se um dos operandos for falso, o resultado será false, independentemente do valor do outro operando.
+Em resumo, a expressão A && B é verdadeira somente se A e B forem verdadeiros.
+O operador || retorna true se pelo menos um dos operandos for verdadeiro.
+Se ambos os operandos forem falsos, o resultado será false.
+Em resumo, a expressão A || B é verdadeira se A for verdadeira, B for verdadeira ou ambos forem verdadeiros.
+*/

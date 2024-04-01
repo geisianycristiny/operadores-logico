db7e92e98b6c45d0317b5bc0d1597bb1bc4dc101
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:50:53 -0400

    adicionado questao 17

---
+++ queatao17.js
@@ -0,0 +1,11 @@
+/*
+17.Explique o que significa a expressão !(10 <= 5).
+(10 <= 5): Esta expressão verifica se 10 é menor ou igual a 5. Como 10 não é menor nem igual a 5, essa expressão é falsa.
+
+!: O operador ! (negação) inverte o valor booleano da expressão que o segue. Ou seja, se a expressão seguinte for verdadeira, a negação a torna falsa, e vice-versa.
+
+Portanto, !(10 <= 5) significa negar o resultado da expressão 10 <= 5, que é falso. Assim, a negação de falso é verdadeiro.
+
+Em resumo, a expressão !(10 <= 5) é verdadeira. Isso ocorre porque estamos negando a afirmação de que 10 é menor ou igual a 5, o que é falso, resultando em verdadeiro.
+
+*/

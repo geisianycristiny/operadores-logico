908be254d8b4baf8115f8c4a133d4613ea9fdac2
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:48:51 -0400

    adicionado questao 28

---
+++ questao28.js
@@ -0,0 +1,13 @@
+/*
+28.Como você pode verificar se uma pessoa é adolescente (idade entre 13 
+e 19 anos) em JavaScript usando operadores lógicos?
+
+var idade = 16; // Idade da pessoa
+
+// Verifica se a pessoa é adolescente (idade entre 13 e 19 anos) usando operadores lógicos
+if (idade >= 13 && idade <= 19) {
+    console.log("A pessoa é adolescente.");
+} else {
+    console.log("A pessoa não é adolescente.");
+}
+*/

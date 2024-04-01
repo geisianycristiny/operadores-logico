3e46e59e5e6156ca708496c7706f8f16ea180cde
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:43:46 -0400

    adicionado questao 9

---
+++ questao9.js
@@ -0,0 +1,5 @@
+/*
+9. O que retorna a expressão false && (true || false)?
+Dentro dos parênteses, temos true || false, que é verdadeiro porque pelo menos um dos operandos é verdadeiro.
+Em seguida, temos false && (resultado de true || false). Como o primeiro operando é false, independentemente do resultado dentro dos parênteses, a expressão resultará em false.
+*/

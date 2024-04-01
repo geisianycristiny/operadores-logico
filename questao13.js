64fffabe6cacb0cda23eaeb3c8ad2c59c870a424
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:44:41 -0400

    adicionado questao 13

---
+++ questao13.js
@@ -0,0 +1,8 @@
+/*
+13.O que significa a expressão !(5 !== 5)?
+5 !== 5: Esta expressão compara se 5 não é estritamente diferente de 5. Como 5 é igual a 5, a expressão 5 !== 5 é falsa.
+
+! (negação): O operador ! nega o valor da expressão que o segue. Se a expressão seguinte for verdadeira, a negação a tornará falsa, e vice-versa.
+
+Portanto, !(5 !== 5) significa negar o resultado de 5 !== 5, que é falso. Assim, a negação de falso é verdadeiro.
+*/

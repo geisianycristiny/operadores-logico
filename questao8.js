82dee9553cdceaedad3e4429b614e6bbc6497b18
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:43:36 -0400

    adicionado questao 8

---
+++ questao8.js
@@ -0,0 +1,13 @@
+/*
+8. Como você pode verificar se um número é par e menor que 100 em 
+JavaScript usando operadores lógicos?
+var numero = 42; // O número que você quer verificar
+
+// Verifica se o número é par e menor que 100 usando operadores lógicos
+if (numero % 2 === 0 && numero < 100) {
+    console.log("O número é par e menor que 100.");
+} else {
+    console.log("O número não é par ou não é menor que 100.");
+}
+
+*/

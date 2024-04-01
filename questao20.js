5bfe57c0f5b976770bbb9ed89769ab527310dd7e
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:47:08 -0400

    adicionado questao 20

---
+++ questao20.js
@@ -0,0 +1,9 @@
+/*
+20.Qual é o resultado de (5 >= 5) && (10 === 10)?
+(5 >= 5) é verdadeiro.
+(10 === 10) é verdadeiro.
+Portanto, verdadeiro && verdadeiro resulta em verdadeiro.
+
+Assim, o resultado da expressão (5 >= 5) && (10 === 10) é verdadeiro.
+
+*/

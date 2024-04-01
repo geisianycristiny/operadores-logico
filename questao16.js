4c0e000f2f9d349a6ac46e4d927487e153e5dae6
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:45:24 -0400

    adicionado questao 16

---
+++ questao16.js
@@ -0,0 +1,8 @@
+/*
+16.Qual é o resultado de (true && true) || (false && false)?
+(true && true) avalia para true, pois ambos os operandos são verdadeiros.
+(false && false) avalia para false, pois ambos os operandos são falsos.
+Agora, aplicamos o operador || (OU lógico) aos resultados das duas expressões:
+
+true || false resulta em true.
+*/

60ad8502606548b07a5d6b95523a37cee38b4a2e
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:44:51 -0400

    adicionado questao 14

---
+++ questao14.js
@@ -0,0 +1,5 @@
+/*
+14.Qual é o resultado de (10 > 5) && !(3 < 1)?
+(10 > 5) é verdadeiro, pois 10 é maior que 5.
+!(3 < 1) é verdadeiro, pois 3 não é menor que 1. O operador ! inverte o resultado da expressão dentro dos parênteses.
+*/

8f8e56e64f6bd5341c0a6be07043effd465aae88
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:43:20 -0400

    adicionado questao 7

---
+++ questao7.js
@@ -0,0 +1,6 @@
+/*
+7. Qual é o resultado de true && (false || true)?
+false || true: Esta expressão retorna true, pois pelo menos um dos operandos é verdadeiro.
+true && (resultado de false || true): Aqui, estamos lidando com o operador && (E lógico), que requer que ambos os operandos sejam verdadeiros para que a expressão total seja verdadeira.
+Como o resultado de false || true é true, a expressão se torna true && true, o que resulta em true.
+*/

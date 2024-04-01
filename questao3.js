0c0bb322e9e11a6b939b3f508cd845f3c9f4d378
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:42:12 -0400

    adicionado questao 3

---
+++ questao3.js
@@ -0,0 +1,9 @@
+/*
+3. Qual é o resultado de !(5 === 3) && (4 >= 4)?
+
+!(5 === 3) é verdadeiro.
+(4 >= 4) é verdadeiro.
+Portanto, verdadeiro && verdadeiro resulta em verdadeiro.
+
+Então, o resultado da expressão !(5 === 3) && (4 >= 4) é verdadeiro.
+*/

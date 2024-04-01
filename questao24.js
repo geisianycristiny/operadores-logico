aea6447f626a3c8f50abc53b90b30680ba7783e6
Ana Eloize <anaeloizea@gmail.com>
2024-04-01 08:48:12 -0400

    adicionado questao 24

---
+++ questao24.js
@@ -0,0 +1,13 @@
+/*
+24.Como você pode verificar se um número é múltiplo de 3 ou 5 em 
+JavaScript usando operadores lógicos?
+
+var numero = 15; // O número que você quer verificar
+
+// Verifica se o número é múltiplo de 3 ou 5 usando operadores lógicos
+if (numero % 3 === 0 || numero % 5 === 0) {
+    console.log("O número é múltiplo de 3 ou 5.");
+} else {
+    console.log("O número não é múltiplo de 3 ou 5.");
+}
+*/
